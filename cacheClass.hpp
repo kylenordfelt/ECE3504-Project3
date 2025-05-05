bbe70212e52cba131b812ecf11685df3b22b1a76
kylenordfelt <kylenordfelt@vt.edu>
2025-05-05 17:13:48 -0400

    Add project files.

---
+++ cacheClass.hpp
@@ -0,0 +1,14 @@
+#ifndef CACHECLASS_HPP
+#define CACHECLASS_HPP
+
+class CacheClass {
+public:
+
+private:
+	// Cache data members
+  // e.g., std::vector<int> cacheData;
+};
+
+
+
+#endif
---
+++ cachesim.cpp
@@ -0,0 +1,10 @@
+#include "cacheClass.hpp"
+
+int main() {
+	// Create an instance of CacheClass
+	CacheClass cache;
+	// Example usage of the cache
+	// cache.loadData(...);
+	// cache.storeData(...);
+	return 0;
+}
